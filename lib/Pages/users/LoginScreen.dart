343b4018295e1982fb3560b983d83c9272537795
Baishalya Roul <baishalya1999@gmail.com>
2023-10-17 15:10:54 +0530

    ui changes

--- lib/Pages/users/LoginScreen.dart
+++ lib/Pages/users/LoginScreen.dart
@@ -174,7 +174,30 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
     );
   }
-
+  // Method to buildsocial media login button
+  Widget _buildSocialButton({VoidCallback? onTap, required String imageAsset}) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 60.0,
+        width: 60.0,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              offset: Offset(0, 2),
+              blurRadius: 6.0,
+            ),
+          ],
+          image: DecorationImage(
+            image: AssetImage(imageAsset),
+          ),
+        ),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     //final viewModel = Provider.of<AuthViewModel>(context);
@@ -218,106 +241,117 @@ class _LoginScreenState extends State<LoginScreen> {
           SingleChildScrollView(
             physics: const AlwaysScrollableScrollPhysics(),
             padding: const EdgeInsets.symmetric(
-              horizontal: 40.0,
-              vertical: 120.0,
+              horizontal: 0.0,
+              vertical: 80.0,
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  "Welcome Back",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 36.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 20),
-                const Text(
-                  "Sign in to continue",
-                  style: TextStyle(
-                    color: Colors.white70,
-                    fontSize: 18.0,
-                  ),
-                ),
-                SizedBox(height: 30),
-                _buildEmailField(emailController),
-                SizedBox(height: 20),
-                _buildPasswordField(passwordController),
-                Container(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                    onPressed: () {
-                      Get.toNamed('/signup');
-                    },
-                    child: Text(
-                      'Forgot Password?',
+            child: Card(
+              color: Colors.transparent,
+              elevation: 5, // Add elevation for a card-like appearance
+              margin: EdgeInsets.all(0),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+
+                  children: <Widget>[
+                    //Header Text
+                    const Text(
+                      "Welcome Back",
                       style: TextStyle(
                         color: Colors.white,
+                        fontSize: 36.0,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                  ),
-                ),
-                SizedBox(height: 20),
-                _buildLoginButton(context,),
-                SizedBox(height: 30),
-                const Text(
-                  'Or Sign in with',
-                  style: TextStyle(
-                    color: Colors.white70,
-                    fontSize: 16.0,
-                  ),
-                ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                  /*  _buildSocialButton(
-                      onTap: () {
-                        // Handle Facebook sign in
-                      },
-                      imageAsset: 'assets/facebook.png',
+                    SizedBox(height: 20),
+                    const Text(
+                      "Sign in to continue",
+                      style: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 18.0,
+                      ),
                     ),
-                    SizedBox(width: 40),*/
-                    _buildSocialButton(
-                      onTap: () {
-                        // Handle Google sign in
-                      },
-                      imageAsset: 'assets/google.png',
-                    ),
-                  ],
-                ),
-                SizedBox(height: 40),
-                GestureDetector(
-                  onTap: () {
-                    // Handle sign up
-                    Get.offNamed('/home');
-                  },
-                  child: RichText(
-                    text: const TextSpan(
-                      children: [
-                        TextSpan(
-                          text: "Don't have an account? ",
-                          style: TextStyle(
-                            color: Colors.white70,
-                            fontSize: 16.0,
-                          ),
-                        ),
-                        TextSpan(
-                          text: "Sign up",
+                    //Text field
+                    const SizedBox(height: 30),
+                    _buildEmailField(emailController),
+                    const SizedBox(height: 1),
+                    _buildPasswordField(passwordController),
+                    Container(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {
+                          Get.toNamed('/signup');
+                        },
+                        child: Text(
+                          'Forgot Password?',
                           style: TextStyle(
                             color: Colors.white,
-                            fontSize: 16.0,
                             fontWeight: FontWeight.bold,
-                            decoration: TextDecoration.underline,
                           ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    _buildLoginButton(context,),
+                    const SizedBox(height: 10),
+                    const Text(
+                      'Or Sign in with',
+                      style: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                      /*  _buildSocialButton(
+                          onTap: () {
+                            // Handle Facebook sign in
+                          },
+                          imageAsset: 'assets/facebook.png',
+                        ),
+                        SizedBox(width: 40),*/
+                        _buildSocialButton(
+                          onTap: () {
+                            // Handle Google sign in
+                          },
+                          imageAsset: 'assets/google.png',
                         ),
                       ],
                     ),
-                  ),
+                    const SizedBox(height: 40),
+                    GestureDetector(
+                      onTap: () {
+                        // Handle sign up
+                        Get.offNamed('/home');
+                      },
+                      child: RichText(
+                        text: const TextSpan(
+                          children: [
+                            TextSpan(
+                              text: "Don't have an account? ",
+                              style: TextStyle(
+                                color: Colors.white70,
+                                fontSize: 16.0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: "Sign up",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.bold,
+                                decoration: TextDecoration.underline,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
         ],
@@ -325,28 +359,6 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
-  Widget _buildSocialButton({VoidCallback? onTap, required String imageAsset}) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        height: 60.0,
-        width: 60.0,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black26,
-              offset: Offset(0, 2),
-              blurRadius: 6.0,
-            ),
-          ],
-          image: DecorationImage(
-            image: AssetImage(imageAsset),
-          ),
-        ),
-      ),
-    );
-  }
+
 
 }
